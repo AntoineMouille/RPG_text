a7ec360a3c5a7417e32f66eb8d844750dd30c167
antoine mouille <antoine_mouille@ens.univ-artois.fr>
2025-09-25 12:57:33 +0200

    :fire: suite projet RPG

---
+++ src/Invetaire.java
@@ -0,0 +1,52 @@
+public class Invetaire {
+    private Hero hero;
+    public final static int MAX_ITEM=20;
+    private Item[] items;
+
+    public Invetaire(Hero hero){
+        this.hero = hero;
+        this.items = new Item[MAX_ITEM];
+    }
+
+    public Hero getHero() {
+        return hero;
+    }
+    public Item[] getItems() {
+        return items;
+    }
+
+    public void addItem(String item){
+        for (int i=0;i<MAX_ITEM;i++){
+            if (this.items[i]==null){
+                this.items[i]=rechercheItem(item);
+                System.out.println("item ajouter");
+                return;
+            }
+        }
+        System.out.println("inventaire plein");
+    }
+
+    public void removeItem(String item){
+        for (int i=0;i<MAX_ITEM;i++){
+            if (this.items[i]==rechercheItem(item)){
+                this.items[i]=null;
+                System.out.println("item supprimer");
+                return;
+            }
+        }
+        System.out.println("item non trouver");
+    }
+
+    public Item rechercheItem(String itemName){
+        for (int i=0;i<MAX_ITEM;i++){
+            if (this.items[i]!=null && this.items[i].toString().equals(itemName)){
+                System.out.println("item trouver");
+                return this.items[i];
+            }
+        }
+        System.out.println("item non trouver");
+        return null;
+    }
+
+
+}
